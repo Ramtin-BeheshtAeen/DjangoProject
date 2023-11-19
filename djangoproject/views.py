cdc7a7fa21b58412aab266a263a6459b391c44f0
Ramtin_BeheshtAeen_Laptop <ramtinba145822@@gmail.com>
2023-11-19 07:24:13 -0600

    transfer views to my app(myFirstSite) and delete it from djangoproject folder

--- djangoproject/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse, JsonResponse
-
-def http_test(request):
-    return HttpResponse("HTTP-Test is Working")
-
-def json_test(request):
-    return JsonResponse({"title":"Ramtin is a great programmer"})
--- myFirstSite/views.py
+++ myFirstSite/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, JsonResponse
+
+def http_test(request):
+    return HttpResponse("HTTP-Test is Working")
+
+def json_test(request):
+    return JsonResponse({"title":"Ramtin is a great programmer"})
