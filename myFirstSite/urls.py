ff7cd06f7e31527fb1bc9446eccd8ee95044640e
Ramtin BeheshtAeen (powershell) <ramtinba145822@gmail.com>
2023-12-06 10:51:06 -0600

    add new variable to myFirstSite/urls.py && and new method(name=) to path in urlpatternslist && add dynamiclink for index/about in the headerfooter.html

--- myFirstSite/urls.py
+++ myFirstSite/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from myFirstSite.views import http_test, json_test, home_view, contact_view, about_view
 
+app_name = "myFirstSite"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view),
-    path('contact', contact_view),
-    path('about', about_view),
+    path('', home_view, name='index'),
+    path('contact', contact_view, name='contact'),
+    path('about', about_view, name="about"),
     path('http-test', http_test),
     path('json-test',json_test)
 ]
