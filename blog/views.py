b3974c59656c83fcbbc4e248871a781abf706427
Ramtin BeheshtAeen (powershell) <ramtinba145822@gmail.com>
2023-12-18 03:12:34 -0600

    add functions for blogs urls to views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def blog_home_view(request):
+    return render(request, "blog/blog-home.html")
+
+def blog_single_view(request):
+    render(request, "blog/blog-single.html")
